e069ede9f37b3570fa3b36abbc1144bef52bf2c1
ElizavetaBlochina <elizabethlizochka@gmail.com>
2024-09-15 22:14:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function () {
+    output.innerHTML = 'Большой привет Лахену и Яше!!!❤';
+        });
+   
